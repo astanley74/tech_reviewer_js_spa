a69c6a52441b76ed0d10ccb3a47e4e10f23233cf
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-02-08 15:36:42 -0500

    add create action to products controller

--- tech_reviewer_backend/app/controllers/api/v1/products_controller.rb
+++ tech_reviewer_backend/app/controllers/api/v1/products_controller.rb
@@ -4,9 +4,14 @@ class Api::V1::ProductsController < ApplicationController
         render json: products
     end
 
+    def create
+        product = Product.create(product_params)
+        render json: product
+    end
+
 private
 
     def product_params
-        params.require(:product).permit(:name, :brand, :price, :avg_rating, :image_url)
+        params.require(:product).permit(:name, :brand, :price, :image_url)
     end
 end
