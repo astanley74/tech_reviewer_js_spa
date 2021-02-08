ea351cce930fae31648b6031f89c13a855a1c96c
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-02-08 02:02:18 -0500

    add h2 with average rating to product and style with css

--- tech_reviewer_frontend/index.js
+++ tech_reviewer_frontend/index.js
@@ -8,13 +8,17 @@ function appendProduct(product) {
     let productDiv = document.querySelector('#product-collection')
     let divCard = document.createElement('div')
     divCard.setAttribute('class', 'product')
+
     let h1 = document.createElement('h1')
-    h1.innerHTML = `${product.brand} ${product.name}`
+    h1.innerHTML = `${product.brand} ${product.name} ($${product.price})`
+
+    let h2 = document.createElement('h2')
+    h2.innerHTML = `Avg Rating: ${product.avg_rating}`
 
     let img = document.createElement('img')
     img.setAttribute('class', 'product-image')
     img.src = product.image_url
 
-    divCard.append(h1, img)
+    divCard.append(h2, img, h1)
     productDiv.append(divCard)
 }
