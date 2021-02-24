c9179af5ebe568f6ece582f996aa376852833fe9
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-02-23 22:25:25 -0500

    add function to render reviews to product class

--- tech_reviewer_frontend/src/product.js
+++ tech_reviewer_frontend/src/product.js
@@ -45,47 +45,51 @@ class Product {
     }
 
     appendProduct() {
-            let productDiv = document.querySelector('#product-collection')
-            let divCard = document.createElement('div')
-            divCard.setAttribute('class', 'product')
-            divCard.dataset.id = this.id
-    
-            let h1 = document.createElement('h1')
-            h1.innerHTML = `${this.brand} ${this.name} ($${this.price})`
-    
-            let h2 = document.createElement('h2')
-            h2.innerHTML = `Avg Rating: ${this.avg_rating}`
-    
-            let img = document.createElement('img')
-            img.setAttribute('class', 'product-image')
-            img.src = this.image_url
+        let productDiv = document.querySelector('#product-collection')
+        let divCard = document.createElement('div')
+        divCard.setAttribute('class', 'product')
+        divCard.dataset.id = this.id
 
-            let reviewDiv = document.createElement('div')
-            reviewDiv.setAttribute('class', 'review')
+        let h1 = document.createElement('h1')
+        h1.innerHTML = `${this.brand} ${this.name} ($${this.price})`
 
-            let newh2 = document.createElement('h3')
-            newh2.innerHTML = "Product Reviews:"
+        let h2 = document.createElement('h2')
+        h2.innerHTML = `Avg Rating: ${this.avg_rating}`
 
-            let addReviewBtn = document.createElement('button')
-            addReviewBtn.innerHTML = "Add Review"
-            addReviewBtn.addEventListener('click', function(){
-                let formDiv = document.createElement('div')
-                formDiv.setAttribute('class', 'review-form')
-                divCard.append(formDiv)
-                Review.createReview(divCard.dataset.id, reviewDiv)
+        let img = document.createElement('img')
+        img.setAttribute('class', 'product-image')
+        img.src = this.image_url
+
+        let reviewDiv = document.createElement('div')
+        reviewDiv.setAttribute('class', 'review')
+
+        let newh2 = document.createElement('h3')
+        newh2.innerHTML = "Product Reviews:"
+
+        let addReviewBtn = document.createElement('button')
+        addReviewBtn.innerHTML = "Add Review"
+        addReviewBtn.addEventListener('click', function(){
+            let formDiv = document.createElement('div')
+            formDiv.setAttribute('class', 'review-form')
+            divCard.append(formDiv)
+            Review.createReview(divCard.dataset.id, reviewDiv)
+        })
+
+
+        reviewDiv.append(newh2)
+
+        divCard.append(h2, img, h1, reviewDiv, addReviewBtn)
+        productDiv.append(divCard)
+
+        this.renderReviews(reviewDiv)
+    }
+
+    renderReviews(reviewDiv) {
+        if (this.reviews) {
+            this.reviews.forEach(review => {
+                let newReview = new Review(review)
+                newReview.appendReview(reviewDiv)
             })
-
-
-            reviewDiv.append(newh2)
-
-            divCard.append(h2, img, h1, reviewDiv, addReviewBtn)
-            productDiv.append(divCard)
-
-            if (this.reviews) {
-                this.reviews.forEach(review => {
-                    let newReview = new Review(review)
-                    newReview.appendReview(reviewDiv)
-                })
-            }
+        }
     }
 }
